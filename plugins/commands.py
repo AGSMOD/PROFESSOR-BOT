6c307feb316446438d87506b10c2df29308b5a5a
AGSMOD <149931558+AGSMOD@users.noreply.github.com>
2023-11-07 23:53:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -202,7 +202,7 @@ async def start(client, message):
     if f_caption is None:
         f_caption = f"{files.file_name}"
     await client.send_cached_media(chat_id=message.from_user.id, file_id=file_id, caption=f_caption, protect_content=True if pre == 'filep' else False,
-                                  reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url=f"https://t.me/piroxbots") ] ] ),)
+                                  reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🤍 MOVIES HUB 🤍', url=f"https://t.me/Movies_Hub_Og") ] ] ),)
     
                     
 
