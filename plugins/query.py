9b4f6156e643dbd371dbc58a9a1fa6b1f01b623b
AGSMOD <149931558+AGSMOD@users.noreply.github.com>
2023-12-04 19:21:16 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -168,10 +168,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         size = get_size(files.file_size)
         f_caption = f_caption = f"{title}"
        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                        file_size='' if size is None else size,
-                                                        file_caption='' if f_caption is None else f_caption)
+           try: f_caption = CUSTOM_FILE_CAPTION.format(mention=query.from_user.mention, file_name='' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)                                                                                                      
+            except Exception as e: logger.exception(e)
             except Exception as e:
                 print(e)
                 f_caption = f_caption
